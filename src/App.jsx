5243358cf5a9a1cb4f077593709bf805a2176907
Yasir Ado Hassan <ysquare.theimperial@gmail.com>
2023-07-10 14:44:52 +0100

    sasve

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Button } from 'reactstrap'
+import PublishRide from './Components/PublishRide'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0) 
 
   return (
     <div>
-      <Button className="">fasdfds</Button>
+      <PublishRide/>
     </div>
   )
 }
